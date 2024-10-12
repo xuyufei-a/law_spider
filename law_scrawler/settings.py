68f100dd9f89abf8246e4da52a5b2a91916cffb8
徐宇飞 <yufeixu20@gmail.com>
2024-10-12 20:44:47 +0800

    final version of sync spider

--- law_scrawler/settings.py
+++ law_scrawler/settings.py
@@ -1,3 +1,4 @@
+import random
 # Scrapy settings for law_scrawler project
 #
 # For simplicity, this file contains only settings considered important or
@@ -11,9 +12,61 @@ BOT_NAME = "law_scrawler"
 
 SPIDER_MODULES = ["law_scrawler.spiders"]
 NEWSPIDER_MODULE = "law_scrawler.spiders"
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 DOWNLOAD_DELAY = 2
 
+USER_AGENT_LIST = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    ]
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
+
+class RandomUserAgentMiddleware:
+    def process_request(self, request, spider):
+        user_agent = random.choice(USER_AGENT_LIST)
+        request.headers['User-Agent'] = user_agent
+
+
+CONCURRENT_REQUESTS = 32
+DOWNLOAD_DELAY = 1
+
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "law_scrawler (+http://www.yourdomain.com)"
@@ -74,7 +127,7 @@ ROBOTSTXT_OBEY = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+#AUTOTHROTTLE_MAX_DELAY = 6CONCURRENT_REQUESTS0
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
--- law_scrawler/spiders/file_spider.py
+++ law_scrawler/spiders/file_spider.py
@@ -5,22 +5,22 @@ from urllib.parse import urljoin
 from selenium import webdriver
 import time
 from selenium.webdriver.support.ui import WebDriverWait
-
-CATE = "xf"
+import re
 
 class Logger():
-    def __init__(self):
-        if os.path.exists(f'result/logs/{CATE}_words_log.txt'):
-            os.remove(f'result/logs/{CATE}_words_log.txt')
+    def __init__(self, category, remove_log):
+        if remove_log and os.path.exists(f'result/logs/{category}_words_log.txt'):
+            os.remove(f'result/logs/{category}_words_log.txt')
         if not os.path.exists('result/logs'):
             os.mkdir('result/logs')
+        self.category = category
 
     def log(self, msg):
-        with open(f'result/logs/{CATE}_words_log.txt', 'a') as f:
+        with open(f'result/logs/{self.category}_words_log.txt', 'a') as f:
             f.write(msg + '\n')
 
     def error(self, msg):
-        with open(f'result/logs/{CATE}_words_log.txt', 'a') as f:
+        with open(f'result/logs/{self.category}_words_log.txt', 'a') as f:
             f.write(f"[ERROR]: {msg}\n")
 
 class FileSpiderSpider(scrapy.Spider):
@@ -28,28 +28,62 @@ class FileSpiderSpider(scrapy.Spider):
     name = "file_spider"
     allowed_domains = ["flk.npc.gov.cn"]
     category = ["xf", "fl", "dfxfg"]
-    start_urls = []
 
-    def __init__(self):
+    def __init__(self, category, resume: bool):
+        resume = resume == 'True'
+        self.my_logger = Logger(category, remove_log=not resume)
+        self.category = category
+        
+        with open(f"result/urls/{category}_urls.txt", 'r') as f:
+            lines = f.readlines()
+        self.urls = [urljoin(self.base_url.format(category), line.strip()) for line in lines]
+
+        if resume and os.path.exists(f"result/logs/{category}_words_log.txt"):
+            with open(f"result/logs/{category}_words_log.txt", 'r') as f:
+                context = f.read()
+            finished_urls = re.findall(r"^downloaded .+? from (.+?)$", context, re.MULTILINE) 
+
+
+            self.urls = [url for url in self.urls if url not in finished_urls]
+
+        self.remaining_urls = len(self.urls)
+    
+    def _get_driver(self, headless):
+        proxy_pool_url = "https://269900.xyz/fetch_https"
+        for i in range(10):
+            try:
+                response = requests.get(proxy_pool_url)
+                proxy = response.text
+
+                if response.status_code != 200:
+                    raise Exception("Failed to fetch proxy")
+                break
+            except Exception as e:
+                self.my_logger.log(f"{i}-th failure to fetch proxy: {e}")
+                pass  
+
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        self.driver = webdriver.Chrome(options=options)
+        options.add_argument(f'--proxy-server={proxy}')
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--allow-insecure-localhost')  # 可选，针对本地自签名证书
+        options.add_argument('--allow-running-insecure-content')  # 允许加载不安全的内容
 
-        self.my_logger = Logger()
-        self.urls = {}
-        for c in self.category:
-            with open(f"result/urls/{c}_urls.txt", 'r') as f:
-                lines = f.readlines()
-            self.urls[c] = [urljoin(self.base_url.format(c), line.strip()) for line in lines]
-        self.start_urls = self.urls[CATE]
+        if headless:
+            options.add_argument('--headless')
+        driver = webdriver.Chrome(options=options)
+        return driver
+ 
+    def start_requests(self):
+        for url in self.urls:
+            yield scrapy.Request(url, callback=self.parse)
 
     def download_file(self, url, category):
-        exit()
-        self.driver.get(url)
+        driver = self._get_driver(True)
+        driver.get(url)
         for i in range(10): 
             try:
-                download_url = WebDriverWait(self.driver, 10).until(
-                    lambda: self.driver.execute_script("return typeof window.getDownLoadUrl !== 'undefined' && getDownLoadUrl();")
+                download_url = WebDriverWait(driver, 10).until(
+                    lambda b: b.execute_script("return typeof window.getDownLoadUrl !== 'undefined' && getDownLoadUrl();")
                 )
 
                 if download_url:
@@ -63,17 +97,21 @@ class FileSpiderSpider(scrapy.Spider):
                         f.write(response.content)
 
                     self.my_logger.log(f"downloaded {download_filename} from {url}")
+                    self.remaining_urls -= 1
+                    self.my_logger.log(f"remaining urls: {self.remaining_urls}")
                 else:
                     raise Exception("Download URL is empty")
                 
                 return 
             except Exception as e:
                 self.my_logger.log(f"{i}-th failure to download {url}: {e}")
-                self.driver.get(url)
+                driver.quit()
+                driver = self._get_driver(True)
+                driver.get(url)
                 time.sleep(2)  
 
         self.my_logger.error(f"Failed to download {url}")
+        time.sleep(10)
 
     def parse(self, response):
-        self.driver.implicitly_wait(10)
-        self.download_file(response.url, CATE)
+        self.download_file(response.url, self.category)
