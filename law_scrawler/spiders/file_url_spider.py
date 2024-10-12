b28d8a38fed1a9da6fec82dc60c5609318862d60
徐宇飞 <yufeixu20@gmail.com>
2024-10-12 12:14:29 +0800

    first version of file spider

---
+++ law_scrawler/spiders/file_url_spider.py
@@ -0,0 +1,72 @@
+import asyncio
+from playwright.async_api import async_playwright
+import requests
+import os
+from urllib.parse import urljoin
+
+class logger():
+    def __init__(self):
+        if(os.path.exists('result/logs/words_log.txt')):
+            os.remove('result/logs/words_log.txt')
+
+    def log(self, msg):
+        with open('result/logs/words_log.txt', 'a') as f:
+            f.write(msg + '\n')
+
+    def error(self, msg):
+        with open('result/logs/words_log.txt', 'a') as f:
+            f.write(f"ERROR: {msg}\n")
+
+class spider():
+    base_url = "https://flk.npc.gov.cn/{}.html"
+    category = ["xf", "fl", "dfxfg"]
+
+    def __init__(self):
+        self.logger = logger()
+        
+        self.urls = {}
+        for c in self.category:
+            with open(f"result/urls/{c}_urls.txt", 'r') as f:
+                lines = f.readlines()
+            self.urls[c] = [urljoin(self.base_url.format(c), line.strip()) for line in lines]
+
+    async def download_file(self, url, catogery):
+        for i in range(3):
+            try:
+                async with async_playwright() as p:
+                    browser = await p.chromium.launch()
+                    page = await browser.new_page()
+
+                    await page.goto(url)
+
+                    download_url = await page.evaluate("getDownLoadUrl();")
+                    download_filename = download_url.split('/')[-1]
+                    if download_url:
+                        response = requests.get(download_url)
+                        
+                        with open(os.path.join(f"result/words/{catogery}", download_filename), 'wb') as f:
+                            f.write(response.content)
+                        self.logger.log(f"downloaded {download_filename} from {url}")
+                    else:
+                        self.logger.log(f"failed to get download url from {url}")                
+                    await browser.close()
+                break
+            except Exception as e:
+                self.logger.error(f"failed to download {url}: {e}")
+    
+    async def download_files(self):
+        asyncio.Semaphore(5)
+
+        tasks = []
+        for catogery, url in self.urls.items():
+            task = asyncio.craate_url(self.download_file(url, catogery))
+            task.append(task)
+
+        return await asyncio.gather(*tasks)
+    
+    def run(self):
+        asyncio.run(self.download_files())
+
+
+if __name__ == '__main__':
+    s = spider()
--- law_scrawler/spiders/my_spider.py
+++ law_scrawler/spiders/my_spider.py
@@ -5,20 +5,29 @@ from scrapy.http import HtmlResponse
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import html
 import time
 import re
 import os
 
 path_dict = {"https://flk.npc.gov.cn/fl.html": "法律"}
+page_num = {"https://flk.npc.gov.cn/fl.html": 68, "https://flk.npc.gov.cn/xf.html": 1, "https://flk.npc.gov.cn/dfxfg.html": 2250}
 
 class MySpiderSpider(scrapy.Spider):
     name = "my_spider"
     allowed_domains = ["flk.npc.gov.cn"]
-    start_urls = ["https://flk.npc.gov.cn/fl.html", "https://flk.npc.gov.cn/xf.html", "https://flk.npc.gov.cn/dfxfg.html"]
+    # start_urls = ["https://flk.npc.gov.cn/fl.html", "https://flk.npc.gov.cn/xf.html", "https://flk.npc.gov.cn/dfxfg.html"]
+    start_urls = ["https://flk.npc.gov.cn/dfxfg.html"]
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        chrome_options = Options()
+        chrome_options.add_argument('--ignore-certificate-errors')
+        chrome_options.add_argument('--allow-insecure-localhost')  # 可选，针对本地自签名证书
+        chrome_options.add_argument('--allow-running-insecure-content')  # 允许加载不安全的内容
+
+
+        self.driver = webdriver.Chrome(chrome_options)
         self.log_file = 'log.txt'
         if os.path.exists(self.log_file):
             os.remove(self.log_file)
@@ -26,80 +35,48 @@ class MySpiderSpider(scrapy.Spider):
     def log(self, msg):
         with open(self.log_file, 'a') as f:
             f.write(msg + '\n')
+    
+    def _scraw_page(self, idx, response):
+        self.log(f"start to crawl {response.url} page {idx}")
+
+        if idx != 1:
+            try:
+                page_input = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, 'span.layui-laypage-skip input.layui-input'))
+                )
+                page_input.clear()
+                page_input.send_keys(str(idx))
+                confirm_button = self.driver.find_element(By.CSS_SELECTOR, 'span.layui-laypage-skip button.layui-laypage-btn')
+                confirm_button.click()
+                time.sleep(1)
+            except Exception as e:
+                self.log(f"failed to jump to page {idx} of {response.url}: {e}")
+                self.driver.get(response.url)
+                self.driver.implicitly_wait(10)
+                time.sleep(5)
+                return False
+
+        rendered_body = self.driver.page_source
+        response = HtmlResponse(url=response.url, body=rendered_body, encoding='utf-8')
+        urls = re.findall("showDetail\(['\"](.+?)['\"]\)", html.unescape(response.text))
+
+        if len(urls) == 0:
+            self.log(f"retrying {response.url} page {idx}")
+            self.driver.refresh()
+            return False
+        else:
+            with open(response.url.split('/')[-1].split('.')[0] + '_urls.txt', 'a') as f:
+                f.write('\n'.join(urls) + '\n')
+            return True
 
     def parse(self, response):
         self.driver.get(response.url)
         self.driver.implicitly_wait(10)
         time.sleep(5)
 
-        idx = 1
-        while True:
-            self.log(f"Start crawling {response.url}, page {idx}") 
-            
-            rendered_body = self.driver.page_source
-            response = HtmlResponse(url=response.url, body=rendered_body, encoding='utf-8')
-
-            urls = re.findall("showDetail\(['\"](.+?)['\"]\)", html.unescape(response.text))
-
-            # with open('text.html', 'w', encoding='utf-8') as f:
-                # f.write(response.text)
-            # print(response.text)
-            # print(urls, len(urls))
-
-            if len(urls) == 0:
-                self.log(f"retrying {response.url}, page {idx}")
-
-                self.driver.refresh()
-                page_input = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, 'span.layui-laypage-skip input.layui-input'))
-                )
-
-                # 输入目标页码，比如第5页
-                page_input.clear()  # 清空输入框
-                page_input.send_keys(str(idx))  # 输入目标页码
-
-                # 找到并点击“确定”按钮
-                confirm_button = self.driver.find_element(By.CSS_SELECTOR, 'span.layui-laypage-skip button.layui-laypage-btn')
-                confirm_button.click()
-                time.sleep(2)
-                continue
-
-            with open(response.url.split('/')[-1].split('.')[0] + '_urls.txt', 'a') as f:
-                f.write('\n'.join(urls) + '\n')
-
-            try:
-                next_button = WebDriverWait(self.driver, 10).until(
-                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.layui-laypage-next'))
-                )
-
-                if "disabled" in next_button.get_attribute("class"):
-                    self.log(f"Finished crawling {response.url}, the last page is {idx}")
-                    break
-
-                next_button.click()
-                time.sleep(2)
-            except:
-                self.log(f"retrying {response.url}, page {idx}")
-
-                self.driver.refresh()
-                page_input = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, 'span.layui-laypage-skip input.layui-input'))
-                )
-
-                # 输入目标页码，比如第5页
-                page_input.clear()  # 清空输入框
-                page_input.send_keys(str(idx))  # 输入目标页码
-
-                # 找到并点击“确定”按钮
-                confirm_button = self.driver.find_element(By.CSS_SELECTOR, 'span.layui-laypage-skip button.layui-laypage-btn')
-                confirm_button.click()
-                time.sleep(2) 
-
-                break
-            
-            idx +=  1
-            
-        self.log(f"Unusual end of {response.url}, page {idx}")
+        for i in range(1, page_num[response.url] + 1):
+            while not self._scraw_page(i, response):
+                pass
     
     def __del__(self):
         self.driver.quit()
